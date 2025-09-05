c2fec4707316426c7297ac6b35967e7a8cd540aa
Richkum <kumatum2@gmail.com>
2025-09-05 11:54:02 +0100

    fixing npm and pnpm conflicts

--- apps/dashboard/src/app/(main)/page.tsx
+++ apps/dashboard/src/app/(main)/page.tsx
@@ -4,22 +4,23 @@ import { useState } from "react";
 import { Button } from "../../core/components/ui/button";
 import { EmptyOrg } from "@/core/components/dashboard/emptyorg";
 import { NewBoardModal } from "@/core/components/dashboard/board/new-baord-modal";
+import { ProtectedRoute } from "@/core/components/auth/RouteGuard";
 
 const DashboardPage = () => {
   const [open, setOpen] = useState(false);
 
   return (
     <>
-      <div className="flex-1 h-[calc(100%-80px)] p-5 flex flex-col items-center justify-center space-y-6">
-        <EmptyOrg />
-        <Button size="lg" onClick={() => setOpen(true)}
-        >
-          + Start New Board
-        </Button>
-      </div>
+      <ProtectedRoute>
+        <div className="flex-1 h-[calc(100%-80px)] p-5 flex flex-col items-center justify-center space-y-6">
+          <EmptyOrg />
+          <Button size="lg" onClick={() => setOpen(true)}>
+            + Start New Board
+          </Button>
+        </div>
 
-      <NewBoardModal open={open} onClose={() => setOpen(false)} />
-
+        <NewBoardModal open={open} onClose={() => setOpen(false)} />
+      </ProtectedRoute>
     </>
   );
 };
